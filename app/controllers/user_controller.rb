328aa85ea6506020752e10b2dd879a6dd622e8fb
abhinayak <abhishek.nayak@mountblue.io>
2018-11-09 16:24:04 +0530

    Removed unnecessary renderings

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,18 +1,14 @@
 class UserController < ApplicationController
   skip_before_action :verify_authenticity_token
-  attr_accessor :users
+
   def index
     users = User.all
     render :json => users
   end
 
   def get
-    begin
       user = User.find(params[:id])
       render :json => user
-    rescue ActiveRecord::RecordNotFound
-      render :json => []
-    end
   end
 
   def create
@@ -22,13 +18,9 @@ class UserController < ApplicationController
   end
 
   def delete
-    begin
       permitted = params.permit(:id)
       user = User.find(params[:id])
       user.destroy if user
       render :json => user
-    rescue ActiveRecord::RecordNotFound
-      render :json => []
-    end
   end
 end
